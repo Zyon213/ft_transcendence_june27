82168160069a20abe5421358bb91c5b1b33b7f99
zyon213 <yg.mms2@gmail.com>
2023-08-30 03:01:39 +0400

    friend page progress

---
+++ src010/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="home">
+    <TopNavBar />
+    <div class="side-cont">
+      <SideNavBar />
+	  <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNavBar from "@/components/TopNavBar.vue";
+import SideNavBar from "@/components/SideNavBar.vue";
+
+export default {
+  name: "homePage",
+  components: {
+    TopNavBar,
+    SideNavBar,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Arial", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+.side-cont {
+  display: flex;
+}
+.prof-cont {
+  flex: 1;
+  padding: 20px;
+}
+</style>
---
+++ src010/components/DropdownMenu.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="dropdown" ref="dropdown">
+    <img
+      class="dropbtn"
+      src="@/assets/game.svg"
+      alt="Dropdown Button"
+      @click="toggleDropdown"
+    />
+    <div
+      class="dropdown-content"
+      :class="{ active: showDropdown }"
+      @click.stop=""
+    >
+      <router-link to="/profile">Edit Profile</router-link>
+      <router-link to="/twofactor">Two-factor-AUTH</router-link>
+      <router-link to="/logout">Logout</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDropdown: false,
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+
+      if (this.showDropdown) {
+        document.addEventListener("click", this.closeDropdownOnClickOutside);
+      } else {
+        document.removeEventListener("click", this.closeDropdownOnClickOutside);
+      }
+    },
+    closeDropdownOnClickOutside(event) {
+      if (!this.$refs.dropdown.contains(event.target)) {
+        this.showDropdown = false;
+        document.removeEventListener("click", this.closeDropdownOnClickOutside);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dropbtn {
+  cursor: pointer;
+  width: 40px;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  right: 100%;
+  margin-right: px;
+}
+
+.active {
+  display: block;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+</style>
